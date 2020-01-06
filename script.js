9ba3b4c6e5d7a4e0b9b00dac895326cbe2f7be2b
Victor <justicejesus1237@gmail.com>
2020-01-06 19:24:46 +0300

    done

---
+++ script.js
@@ -0,0 +1,31 @@
+let images = document.querySelectorAll('.infinity-slider img');
+let current = 0;
+
+
+function slider() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.add('opacity0');
+  }
+  images[current].classList.remove('opacity0');
+
+}
+
+slider();
+
+// document.querySelector('.infinity-slider').onclick = slider;
+document.querySelector('.arrow-left').onclick = function () {
+  if (current - 1 == -1) {
+    current = images.length - 1;
+  } else {
+    current--;
+  }
+  slider();
+};
+document.querySelector('.arrow-right').onclick = function () {
+  if (current + 1 == images.length) {
+    current = 0;
+  } else {
+    current++;
+  }
+  slider();
+};
